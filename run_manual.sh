e6d2463efa53fb6a70863945a76dd880a06a3e0f
Mark Tran <mark.tran@landregistry.gsi.gov.uk>
2015-09-18 13:38:24 +0100

    Update run_manual.sh

--- run_manual.sh
+++ run_manual.sh
@@ -3,3 +3,4 @@
 vagrant ssh nodea -c 'sudo /vagrant/manual_install_master.sh'
 vagrant ssh nodeb -c 'sudo /vagrant/manual_install_node.sh'
 vagrant ssh nodec -c 'sudo /vagrant/manual_install_node.sh'
+vagrant ssh bart -c 'sudo /vargrant/manual_install_bart.sh'
